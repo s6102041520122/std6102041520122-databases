df993369f8e61e4aea96c3e3ba4a23659b1e7a6e
s6102041520122 <s6102041520122@email.kmutnb.ac.th>
2019-04-03 22:20:41 +0700

    MySQL + Worbrnch (GUI)

--- property_for_rent.sql
+++ property_for_rent.sql
@@ -1,10 +1,9 @@
-DROP TABLE property_for_rent;
 -- phpMyAdmin SQL Dump
 -- version 4.6.6
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Mar 27, 2019 at 05:42 PM
+-- Generation Time: Apr 03, 2019 at 03:20 PM
 -- Server version: 5.7.17-log
 -- PHP Version: 5.6.30
 
@@ -29,16 +28,16 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `property_for_rent` (
   `id` int(11) NOT NULL,
-  `property_no` varchar(5) NOT NULL,
+  `property_no` int(11) NOT NULL,
   `street` varchar(20) DEFAULT NULL,
   `city` varchar(20) DEFAULT NULL,
   `postcode` varchar(10) DEFAULT NULL,
   `type` varchar(10) DEFAULT NULL,
   `rooms` int(2) DEFAULT NULL,
   `rent` int(4) DEFAULT NULL,
-  `owner_no` varchar(5) NOT NULL,
-  `staff_no` varchar(5) DEFAULT NULL,
-  `branch_no` varchar(5) NOT NULL
+  `owner_id` int(11) NOT NULL,
+  `staff_no` int(11) DEFAULT NULL,
+  `branch_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -49,7 +48,8 @@ CREATE TABLE `property_for_rent` (
 -- Indexes for table `property_for_rent`
 --
 ALTER TABLE `property_for_rent`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `owner_id_idx` (`owner_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -60,6 +60,16 @@ ALTER TABLE `property_for_rent`
 --
 ALTER TABLE `property_for_rent`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `property_for_rent`
+--
+ALTER TABLE `property_for_rent`
+  ADD CONSTRAINT `owner_id` FOREIGN KEY (`owner_id`) REFERENCES `private_owner` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
